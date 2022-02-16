518c9605e38ea51ceab4e718c8e1657c279c2602
Crusader0926 <98358681+Crusader0926@users.noreply.github.com>
2022-02-15 20:08:01 -0600

    Checks for file

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import urllib.request
 import os.path
 # Imports libraries needed to do requests and things
+
+if not(os.path.isfile('awslog.log')):  
+    print('File not found. Downloading.')
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    urllib.request.urlretrieve(url, 'awslog.log')
+    print('File downloaded. Resuming.')
+    # checks for file, downloads if not there
+    
